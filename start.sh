289df231773b5e6c3fc99568f50140aa130a1bc7
Neeraj Srivastav <50608116+Neeraj-Nec@users.noreply.github.com>
2025-05-17 10:28:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Running docker compose from /home/ec2-user/app"
+
+cd /home/ec2-user/app
+
+# Debug: list files
+ls -la
+
+# Start the container
 sudo docker compose up -d
